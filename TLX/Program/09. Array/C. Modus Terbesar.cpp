fc1ed2c2dde1e0cf4f609624126ac7e46256580f
hebatmandor <hebatmandor@gmail.com>
2020-09-08 14:18:24 +0800

    CPP

--- TLX/Program/09. Array/C. Modus Terbesar.cpp
+++ TLX/Program/09. Array/C. Modus Terbesar.cpp
@@ -1,44 +1,89 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define ar array
-void solve(int N){
-    int arr[N], modus, m, count=1;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> arr[i];
-    }
-    modus=arr[0];
-    for (int i = 0; i < N; i++)
-    {
-        if(arr[i]>0){
-            count = 1;
-            for (int j = i+1; j < N; j++)
-            {
-                if(arr[i]==arr[j]){
-                    arr[j] = 0;
-                    count++;
-                }
+#include<iostream>
+using namespace std; 
+int x;
+
+void findmod(float bil[], int n, float mod[]){
+    int total[100];
+    int k=1;
+    x=0;
+    //untuk mengurutkan secara ascending
+    for(int c=0;c<n;c++){
+        for(int i=(n-1);i>=0;i--){
+            if(bil[i]<bil[i-1]){
+                int temp;
+                temp=bil[i];
+                bil[i]=bil[i-1];
+                bil[i-1]=temp;
             }
-            if(count>m){
-                m=count;
-                modus=arr[i];
+        }
+    }
+//menghitung berapa kali muncul tiap angka
+    for(int c=0;c<n;c++){
+        total[c]=0;
+        for(int i=0;i<n;i++){
+            if(bil[c]==bil[i]){
+                total[c]++;
+            }
+        }
+    }
+//menentukan nilai yang paling sering muncul
+    for(int c=0;c<n;c++){
+        if(total[c]>k){
+            k=total[c];
+        }
+    }
+    //jika modus lebih dari satu
+    for(int c=0;c<n;c++){
+        if(x==0)
+            mod[x]=0;
+        else
+            mod[x]=mod[x-1];
+        if(total[c]==k){
+            if(bil[c]!=mod[x]){
+                mod[x]=bil[c];
+                x++;
+            }
+        }
+    }
+
+    //Jika Semua angka muncul sama banyak
+    int z=0;
+    for(int c=0;c<n;c++){
+        if(total[c]==k){
+            z++;
+        }
+    }
+    if(z==n){
+        x=0;
+    }
+}
+int main(){
+    int n,modus;
+    float bil[100];
+    float mod[100];
+    //cout<<"Banyak N : ";
+    cin>>n;
+    for(int c=0;c<n;c++){ 
+        //cout<<"Nilai "<<(c+1)<<" : ";
+        cin>>bil[c];
+    }
+    //cout<<endl;
+    findmod(bil,n,mod);
+    // if (x==0)
+    // cout<<"Tidak Ada Modus!"<<endl;
+    // else
+    // {
+    // cout<<"Modus : ";
+    for(int c=0;c<x;c++){
+        for (int i = c+1; i <= x; i++)
+        {
+            if(mod[c]>mod[i]){
+                modus = mod[c];
             }else{
-                if(count==m){
-                    if(arr[i]>modus){
-                        m=count;
-                        modus=arr[i];
-                    }
-                }    
+                modus = mod[i];
             }
         }
     }
     cout << modus << endl;
-}
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int N;
-    cin >> N;
-    solve(N);
+
 }
