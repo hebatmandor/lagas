3c6ca64b851732d49ac35db3ad1742e76fff9da1
hebatmandor <hebatmandor@gmail.com>
2020-09-18 20:31:20 +0800

    CPP

---
+++ TLX/Kompetitif Dasar/01. Perkenalan Pemrograman Kompetitif/A. Seleksi Olimpiade.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int n, int m){
+    string cek, id[80];
+    int arrA[80], arrB[80], arrC[80];
+    cin >> cek;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> id[i] >> arrA[i] >> arrB[i] >> arrC[i];
+    }
+    
+}
+
+int main(){
+    int T, N, M;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> N >> M;
+        solve(N, M);
+    }
+}
--- TLX/Program/09. Array/C. Modus Terbesar.cpp
+++ TLX/Program/09. Array/C. Modus Terbesar.cpp
@@ -1,89 +1,35 @@
-#include<iostream>
-using namespace std; 
-int x;
-
-void findmod(float bil[], int n, float mod[]){
-    int total[100];
-    int k=1;
-    x=0;
-    //untuk mengurutkan secara ascending
-    for(int c=0;c<n;c++){
-        for(int i=(n-1);i>=0;i--){
-            if(bil[i]<bil[i-1]){
-                int temp;
-                temp=bil[i];
-                bil[i]=bil[i-1];
-                bil[i-1]=temp;
-            }
-        }
-    }
-//menghitung berapa kali muncul tiap angka
-    for(int c=0;c<n;c++){
-        total[c]=0;
-        for(int i=0;i<n;i++){
-            if(bil[c]==bil[i]){
-                total[c]++;
-            }
-        }
-    }
-//menentukan nilai yang paling sering muncul
-    for(int c=0;c<n;c++){
-        if(total[c]>k){
-            k=total[c];
-        }
-    }
-    //jika modus lebih dari satu
-    for(int c=0;c<n;c++){
-        if(x==0)
-            mod[x]=0;
-        else
-            mod[x]=mod[x-1];
-        if(total[c]==k){
-            if(bil[c]!=mod[x]){
-                mod[x]=bil[c];
-                x++;
-            }
-        }
-    }
-
-    //Jika Semua angka muncul sama banyak
-    int z=0;
-    for(int c=0;c<n;c++){
-        if(total[c]==k){
-            z++;
-        }
-    }
-    if(z==n){
-        x=0;
-    }
-}
+#include <bits/stdc++.h>
+using namespace std;
 int main(){
-    int n,modus;
-    float bil[100];
-    float mod[100];
-    //cout<<"Banyak N : ";
-    cin>>n;
-    for(int c=0;c<n;c++){ 
-        //cout<<"Nilai "<<(c+1)<<" : ";
-        cin>>bil[c];
+    long int arr[100000], count=1, l, modus, n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    //cout<<endl;
-    findmod(bil,n,mod);
-    // if (x==0)
-    // cout<<"Tidak Ada Modus!"<<endl;
-    // else
-    // {
-    // cout<<"Modus : ";
-    for(int c=0;c<x;c++){
-        for (int i = c+1; i <= x; i++)
-        {
-            if(mod[c]>mod[i]){
-                modus = mod[c];
+
+    modus = arr[0], l = 1;
+
+    for (int i = 0; i < n-1; i++){
+        if (arr[i] > 0){
+            count = 1;
+            for (int j = i+1; j < n; j++){
+                if (arr[i] == arr[j]){
+                    arr[j] = 0;
+                    count++;
+                }
+            }
+            if (count > 1){
+                l = count;
+                modus = arr[i];
             }else{
-                modus = mod[i];
+                if (count == 1){
+                    if (arr[i] > modus){
+                        l = count;
+                        modus = arr[i];
+                    }
+                }
             }
         }
     }
     cout << modus << endl;
-
 }
